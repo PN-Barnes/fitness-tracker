f890d9f9fc8ce7e8645a8080ba0c05156e48264c
Paul N Barnes <paulieb99@gmail.com>
2021-08-15 08:00:01 -0700

    added connection functionality to server.js

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
