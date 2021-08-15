209feb54b34bcc2c1caaefa9485b8086aedab111
Paul N Barnes <paulieb99@gmail.com>
2021-08-15 08:06:41 -0700

    added requires for server.js such as models folder and controllers

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const routes = require('./controllers');
 
 const PORT = process.env.PORT || 3000;
+
+const db = require('./models');
 
 const app = express();
 
@@ -10,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
