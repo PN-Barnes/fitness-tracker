7abc6936103e686b711227c8eaaefea99f9ada35
Paul N Barnes <paulieb99@gmail.com>
2021-08-15 13:59:54 -0700

    reworked server js to use mongojs

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const mongojs = require('mongojs');
+const mongojs = require('mongojs');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 
-const db = require('./models');
+const Workout = require('./models/workout');
 const app = express();
 
 app.use(logger('dev'));
@@ -15,15 +15,15 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-// const databaseUrl = 'fitnessTracker';
-// const collections = ['workouts'];
-// const db = mongojs(databaseUrl, collections);
+const databaseUrl = 'fitnessTracker';
+const collections = ['workouts'];
+const db = mongojs(databaseUrl, collections);
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
 });
 
-// app.get('/');
+app.get('/');
 
 // ? Port is listening //
 app.listen(PORT, () => {
