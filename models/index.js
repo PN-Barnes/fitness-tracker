942174e998c540f4d42cf7f2949c942cc91437bf
Paul N Barnes <paulieb99@gmail.com>
2021-08-15 10:26:25 -0700

    fixed syntax errors

--- models/index.js
+++ models/index.js
@@ -1,1 +1,1 @@
-module.exports = Workout: require('./workout')
+module.exports = { Workout: require('./workout') };
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-const routes = require('./controllers');
 
 const PORT = process.env.PORT || 3000;
 
