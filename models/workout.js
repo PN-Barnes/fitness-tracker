b8b60b54c0067b7467fd565f6e73ffb9e0686467
Paul N Barnes <paulieb99@gmail.com>
2021-08-15 08:26:22 -0700

    attempting to configure models

--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({});
+const WorkoutSchema = new Schema({
+  day: Date,
+  exercises: Array,
+});
 
 const Workout = mongoose.model('Workout', WorkoutSchema);
 
